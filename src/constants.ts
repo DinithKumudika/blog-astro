96babafb0ead9498dd779cc7b701d057467e8776
Dinith Kumudika <dinith@linearsix.com>
2024-05-23 23:57:56 +0530

    components and pages added

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "Dinith's Blog";
+export const SITE_DESCRIPTION = "tech blogs related to IT/SE/AI";
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
